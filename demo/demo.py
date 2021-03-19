ad5c1300ee80a7cf67712982edc7f9ac165cfba6
Koouk <koouk99@gmail.com>
2021-03-19 01:32:23 +0100

    GUI v 0.1

--- demo/demo.py
+++ demo/demo.py
@@ -1,25 +1,145 @@
-from ..tomograf.tomograf import *
-from skimage import io
-import numpy as np
-import matplotlib.pyplot as plt
-import time
+import copy
+import sys
+import os
+import skimage.io
+from pydicom import dcmread, uid
+from pydicom.dataset import Dataset, FileDataset, FileMetaDataset
+import datetime
+from fnmatch import fnmatch
 
-start = time.time()
+from pydicom.uid import generate_uid
 
-tomograf = ManyEmitterTomograf(receiver_count=180, angular_dist=180, scans_no=380)
-#tomograf.setAngle(89)
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
-#tomograf.dbg_print_rc_em(10, 0, 0)
-#print(tomograf)
+from tomograf.tomograf import *
+import streamlit as st
 
-img = skimage.io.imread("tomograf/test/Kwadraty2.jpg", as_gray=True)
 
-tomograf.load_image(img)
+@st.cache
+def findJPG(comp):
+    found = []
+    for directory_in_str in comp:
+        directory = os.fsencode(directory_in_str)
+        for path, _, files in os.walk(directory):
+            for name in files:
+                if fnmatch(name.decode("utf-8"), "*.jpg") or fnmatch(name.decode("utf-8"), "*.dcm"):
+                    found.append(os.path.join(path, name).decode("utf-8"))
 
-fig, (sino, recon) = plt.subplots(1, 2)
-sino.imshow(np.array(tomograf.construct_sinogram()), cmap='gray')
-recon.imshow(np.array(tomograf.construct_image()), cmap='gray')
+    return found
 
-end = time.time()
-print("Wall time " + str(end - start))
-plt.show()
+
+@st.cache
+def simulate(receiverCount, angularDist, scansNo, link, filteringA):
+
+    tomograf = ManyEmitterTomograf(receiverCount, angularDist, scansNo)
+    imie_pacjenta = ""
+    id_pacjenta = ""
+    comms = ""
+    if fnmatch(link, "*.jpg"):
+        image = skimage.io.imread(link, as_gray=True)
+
+    else:
+        ds = dcmread(link)
+        image = ds.pixel_array
+        imie_pacjenta = ds.PatientName
+        id_pacjenta = ds.PatientID
+        comms = ds.ImageComments
+
+    tomograf.load_image(image)
+    sinogram = tomograf.construct_sinogram(filteringA)
+    constructedImage, ms, gif = tomograf.construct_image()
+    return image, sinogram, constructedImage, ms, ds, gif
+
+st.title("Tomograf")
+st.sidebar.title('Options')
+receiver_count = st.sidebar.number_input('Receiver Count', value=180, min_value=1, max_value=360)
+angular_dist = st.sidebar.number_input('Angular Distance', value=180, min_value=1, max_value=360)
+scans_no = st.sidebar.number_input('Scans number', value=380, min_value=1, max_value=500)
+filtering = st.sidebar.checkbox('Use basic filter?', value=True)
+gifStep = st.sidebar.number_input('Step', value=10, min_value=1, max_value=500)
+
+dir = [
+    "."
+]
+link = st.selectbox("Dej linka", findJPG(dir))
+
+image, sinogram, constructedImage, ms, dsOld, gifOld = simulate(receiver_count, angular_dist,
+                                                                                   scans_no,
+                                                                                   link, filtering)
+patientName = copy.deepcopy(dsOld.PatientName)
+patientID = copy.deepcopy(dsOld.PatientID)
+comms = copy.deepcopy(dsOld.ImageComments)
+gif = copy.deepcopy(gifOld)
+gif = gif[::gifStep]
+
+patientName = st.text_input('Imie i nazwisko', value=patientName)
+patientID = st.text_input('Id', value=patientID)
+comms = st.text_input('Komentarz ', value=comms)
+
+col1, col2, col3,col4 = st.beta_columns(4)
+with col1:
+    st.header("Initial image")
+    st.image(image, use_column_width=True)
+with col2:
+    st.header("Sinogram")
+    st.image(sinogram, use_column_width=True)
+with col3:
+    st.header("Progress")
+    s = st.slider('Progress?', 0, len(gif) - 1, 0)
+    st.image(gif[s], use_column_width=True)
+with col4:
+    st.header("Processed image")
+    st.image(constructedImage, use_column_width=True)
+st.write("MSE: {}".format(ms))
+
+filename = st.text_input('Nazwa pliku ')
+
+if st.button("Save to file"):
+    file = open(filename + ".dcm", "wb")
+
+    #From scratch
+    # Populate required values for file meta information
+    file_meta = FileMetaDataset()
+    file_meta.MediaStorageSOPClassUID = '1.2.840.10008.5.1.4.1.1.2'
+
+    file_meta.MediaStorageSOPInstanceUID = generate_uid()
+    file_meta.ImplementationClassUID = '1.2.826.0.1.3680043.8.498.'
+
+    file_meta.ImplementationVersionName = "PYDICOM 2.0.0"
+    file_meta.FileMetaInformationGroupLength = 206
+
+    ds = FileDataset(file, {}, file_meta=file_meta, preamble=b"\0" * 128)
+
+    ds.PatientName = patientName
+    ds.PatientID = patientID
+    ds.ImageComments = comms
+    ds.file_meta.TransferSyntaxUID = uid.ImplicitVRLittleEndian
+    ds.PixelData = bytes(image)
+    ds.PixelRepresentation = dsOld.PixelRepresentation
+    ds.BitsAllocated = dsOld.BitsAllocated
+    ds.Rows = dsOld.Rows
+    ds.Columns = dsOld.Columns
+    ds.SamplesPerPixel = dsOld.SamplesPerPixel
+    ds.PhotometricInterpretation = dsOld.PhotometricInterpretation
+    # Set the transfer syntax
+    ds.is_little_endian = True
+    ds.is_implicit_VR = True
+
+    #with template
+
+    #ds = copy.deepcopy(dsOld)
+    #ds.PatientName = patientName
+    #ds.PatientID = patientID
+    #ds.ImageComments = comms
+
+
+    # Set creation date/time
+    dt = datetime.datetime.now()
+    ds.ContentDate = dt.strftime('%Y%m%d')
+    timeStr = dt.strftime('%H%M%S.%f')  # long format with micro seconds
+    ds.ContentTime = timeStr
+
+    ds.save_as(file)
+    print("File saved.")
+    file.close()
--- tomograf/tomograf.py
+++ tomograf/tomograf.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 import matplotlib.pyplot as plt
 from . import filtering
 
+
 class AbstractTomograf(ABC):
     def __init__(self, receiver_count, angular_dist, scans_no=180):
         if receiver_count < 1:
@@ -26,13 +27,15 @@ class AbstractTomograf(ABC):
         self.rotation += self.step % 360
 
     def get_receiver_pos(self, radius, offx, offy, receiver_no):
-        x = radius * np.cos(np.pi + np.deg2rad(self.rotation - self.angular_dist / 2 + receiver_no * self.angular_dist/(self.receiver_count - 1) )  )
-        y = radius * np.sin(np.pi + np.deg2rad(self.rotation - self.angular_dist / 2 + receiver_no * self.angular_dist/(self.receiver_count - 1) )  )
+        x = radius * np.cos(np.pi + np.deg2rad(
+            self.rotation - self.angular_dist / 2 + receiver_no * self.angular_dist / (self.receiver_count - 1)))
+        y = radius * np.sin(np.pi + np.deg2rad(
+            self.rotation - self.angular_dist / 2 + receiver_no * self.angular_dist / (self.receiver_count - 1)))
 
         return (offx + x, offy + y)
 
     def get_receivers(self, radius, offx, offy):
-        return [self.get_receiver_pos(radius, offx, offy, x) for x in np.arange(0, self.receiver_count - 1) ]
+        return [self.get_receiver_pos(radius, offx, offy, x) for x in np.arange(0, self.receiver_count - 1)]
 
     @abstractmethod
     def get_emitter_pos(self, radius, offx, offy, emitter_no):
@@ -43,7 +46,8 @@ class AbstractTomograf(ABC):
         pass
 
     def get_beams(self, radius, offx, offy):
-        return [skimage.draw.line_nd(em, rc) for em, rc in zip(self.get_emitters(radius, offx, offy), self.get_receivers(radius, offx, offy))]
+        return [skimage.draw.line_nd(em, rc) for em, rc in
+                zip(self.get_emitters(radius, offx, offy), self.get_receivers(radius, offx, offy))]
 
     # debug
     def dbg_print_rc_em(self, radius, offx, offy):
@@ -59,12 +63,12 @@ class AbstractTomograf(ABC):
         self.cached_beams = []
 
     def normalize_image(self, frame):
-        frame = (frame - frame.min())/(frame.max() - frame.min())
+        frame = (frame - frame.min()) / (frame.max() - frame.min())
         return frame
 
     def construct_sinogram_frame(self):
         height, width = self.data.shape
-        radius = np.sqrt(height**2 + width**2)/2
+        radius = np.sqrt(height ** 2 + width ** 2) / 2
         frame = []
         beams = self.get_beams(radius, width / 2, height / 2)
         translated_beams = []
@@ -74,63 +78,68 @@ class AbstractTomograf(ABC):
             beams_yy = np.array(beam[1])
 
             # https://thispointer.com/delete-elements-from-a-numpy-array-by-value-or-conditions-in-python/
-            mask = (beams_xx[:] >= 0 )   & (beams_yy[:] >= 0) & (
+            mask = (beams_xx[:] >= 0) & (beams_yy[:] >= 0) & (
                     beams_xx[:] < width) & (beams_yy[:] < height)
 
             beams_xx = beams_xx[mask]
             beams_yy = beams_yy[mask]
 
-            #self.data[(beams_yy, beams_xx)] = 1
-            #plt.imshow(self.data, cmap='gray')
-            #plt.show()
+            # self.data[(beams_yy, beams_xx)] = 1
+            # plt.imshow(self.data, cmap='gray')
+            # plt.show()
 
             translated_beams.append([beams_yy, beams_xx])
-            frame.append(np.sum(self.data[ (beams_yy, beams_xx ) ]))
+            frame.append(np.sum(self.data[(beams_yy, beams_xx)]))
 
         self.cached_beams.append(translated_beams)
 
         return frame
 
-    def construct_sinogram(self):
+
+    def construct_sinogram(self, enable):
         i = 0
         frames = []
         fil = filtering.get_filter(10)
 
         while i < self.scans_no:
             row = self.construct_sinogram_frame()
-            row = np.convolve(row, fil, mode='same')
+            if enable:
+                row = np.convolve(row, fil, mode='same')
             frames.append(row)
             self.tick()
-            i+=1
-            print("skan: {0}/{1}".format(i, self.scans_no), end = "\r")
+            i += 1
+            print("skan: {0}/{1}".format(i, self.scans_no), end="\r")
         print()
 
         self.sinogram = frames
-        
+
         frames = self.normalize_image(np.array(frames))
         return frames
 
     def construct_image(self):
         height, width = self.data.shape
         frame = np.zeros((height, width))
-
+        gif = []
         if not self.cached_beams:
             raise NotImplementedError("You need to construct sinogram from an image first.")
 
         for scanset, beams in zip(self.sinogram, self.cached_beams):
             for beam_val, beam_translated in zip(scanset, beams):
-                frame[(beam_translated[0], beam_translated[1] )] += beam_val
-            #plt.imshow(frame, cmap='gray')
-            #plt.show()
+                frame[(beam_translated[0], beam_translated[1])] += beam_val
+
+
+            # plt.imshow(frame, cmap='gray')
+            # plt.show()
+            gif.append(self.normalize_image(frame))
 
         frame = self.normalize_image(frame)
         MSE = np.mean(np.power(frame - self.data, 2))
         print("MSE: {0}".format(MSE))
-        return frame
+        return frame, MSE, gif
 
     # debug
     def __str__(self):
-        return ','.join([str(x) for x in [self.receiver_count, self.angular_dist, self.step] ])
+        return ','.join([str(x) for x in [self.receiver_count, self.angular_dist, self.step]])
 
 
 class OneEmitterTomograf(AbstractTomograf):
@@ -144,7 +153,8 @@ class OneEmitterTomograf(AbstractTomograf):
         return (offx + x, offy + y)
 
     def get_emitters(self, radius, offx, offy):
-        return [self.get_emitter_pos(radius, offx, offy) for x in np.arange(0, self.receiver_count - 1) ]
+        return [self.get_emitter_pos(radius, offx, offy) for x in np.arange(0, self.receiver_count - 1)]
+
 
 class ManyEmitterTomograf(AbstractTomograf):
     def __init__(self, *args, **kwargs):
@@ -152,9 +162,11 @@ class ManyEmitterTomograf(AbstractTomograf):
         self.emitter_count = self.receiver_count
 
     def get_emitter_pos(self, radius, offx, offy, receiver_no):
-        x = -1 * radius * np.cos(np.pi + np.deg2rad(self.rotation - self.angular_dist / 2 + receiver_no * self.angular_dist/(self.receiver_count - 1) )  )
-        y = -1 * radius * np.sin(np.pi + np.deg2rad(self.rotation - self.angular_dist / 2 + receiver_no * self.angular_dist/(self.receiver_count - 1) )  )
+        x = -1 * radius * np.cos(np.pi + np.deg2rad(
+            self.rotation - self.angular_dist / 2 + receiver_no * self.angular_dist / (self.receiver_count - 1)))
+        y = -1 * radius * np.sin(np.pi + np.deg2rad(
+            self.rotation - self.angular_dist / 2 + receiver_no * self.angular_dist / (self.receiver_count - 1)))
         return ((offx + x), (offy + y))
 
     def get_emitters(self, radius, offx, offy):
-        return [self.get_emitter_pos(radius, offx, offy, x) for x in np.arange(0, self.emitter_count - 1) ][::-1]
+        return [self.get_emitter_pos(radius, offx, offy, x) for x in np.arange(0, self.emitter_count - 1)][::-1]
